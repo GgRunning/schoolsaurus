ba2866be7155690637ae9b838f21acdb4e30a51b
GgRunning <garyleeyq@gmail.com>
2018-03-29 08:20:13 +0800

    updated readme  and settings file to schoolsaurus

--- src/schoolsaurus/wsgi.py
+++ src/schoolsaurus/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for schoolpedia project.
+WSGI config for schoolsaurus project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schoolpedia.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schoolsaurus.settings")
 
 application = Sentry(get_wsgi_application())
 application = DjangoWhiteNoise(application)
